c0a2d1e7a64047d269d509c059d5101ba3117dac
John Beale <7452231+jbeale1@users.noreply.github.com>
2022-12-25 23:09:58 -0800

    process to separate + and - motion direction

--- IQ-plot1.py
+++ IQ-plot1.py
@@ -14,16 +14,41 @@ from scipy.fft import fftshift
 import matplotlib.pyplot as plt
 
 # wav file imports as Nx2 array of signed INT16
-fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk3.wav"
+
+#fname_in="C:/Users/beale/Documents/Audio/Cornercube-run1.wav"
+#fname_in="C:/Users/beale/Documents/Audio/Cornercube-run2.wav"
+#fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk3.wav"
+fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk2.wav"
 
 fs, datraw = scipy.io.wavfile.read(fname_in)
 N,ch = datraw.shape
 xR = datraw.astype(np.float32) / 65535.0
 x = xR[:,0] + 1j * xR[:,1]  # make complex from 2 reals
 
-NFFT=4096
+NFFT=2048
 Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=fs, 
-                                    noverlap=256, scale='dB',
+                                    noverlap=128, scale='dB',
                                     cmap='magma')
-plt.axis((None, None, -1500, 1500))
+plt.axis((None, None, -3000, 3000))  # freq limits, Hz
 plt.show()
+
+c=int(NFFT/2) # center frequency (f=0)
+r=100  # range on either size of f=0
+A = 3
+lm = -1.5 # clamp values below this level
+plt.rc('figure', figsize=[46.82 * .5**(.5 * A), 33.11 * .5**(.5 * A)])
+#s1 = np.log10(Pxx[c-r:c+r,38])
+sum = np.sum(np.log10(Pxx[c-r:c+r,37:43]),axis=1)/6.0
+sumf = np.flip(sum)
+
+plt.grid()
+
+p1 = np.log10(Pxx[c-r:c+r,:])
+p1f = np.flip(p1,0)  # flip array along 1st axis
+pdif = p1f - (p1*0.8)
+p2 = np.maximum(lm,pdif)
+plt.imshow(p2,cmap='magma')
+plt.show
+
+# colormap names   magma plasma  bone
+# stackoverflow.com/questions/66539861/where-is-the-list-of-available-built-in-colormap-names
