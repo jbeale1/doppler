3e1b5509529fac08823d7409d28b11e90ebce674
John Beale <7452231+jbeale1@users.noreply.github.com>
2022-12-26 08:42:51 -0800

    Update IQ-plot1.py

--- IQ-plot1.py
+++ IQ-plot1.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 """
 Plot Radar I/Q data on spectrogram
 Python code with scipy, numpy, matplotlib
-Dec 25 18:39:25 2022
-@author: jbeale
+J.Beale, Dec 26 2022
 """
 
 import numpy as np
@@ -13,12 +11,13 @@ import scipy.io
 from scipy.fft import fftshift
 import matplotlib.pyplot as plt
 
-# wav file imports as Nx2 array of signed INT16
+# stereo wav file is Nx2 array of signed INT16
 
 #fname_in="C:/Users/beale/Documents/Audio/Cornercube-run1.wav"
 #fname_in="C:/Users/beale/Documents/Audio/Cornercube-run2.wav"
 #fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk3.wav"
-fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk2.wav"
+#fname_in="C:/Users/beale/Documents/Audio/DopplerIQ-walk2.wav"
+fname_in="C:/Users/beale/Documents/Audio/Robotwalk1.wav"
 
 fs, datraw = scipy.io.wavfile.read(fname_in)
 N,ch = datraw.shape
@@ -34,19 +33,16 @@ plt.show()
 
 c=int(NFFT/2) # center frequency (f=0)
 r=100  # range on either size of f=0
-A = 3
-lm = -1.5 # clamp values below this level
-plt.rc('figure', figsize=[46.82 * .5**(.5 * A), 33.11 * .5**(.5 * A)])
-#s1 = np.log10(Pxx[c-r:c+r,38])
-sum = np.sum(np.log10(Pxx[c-r:c+r,37:43]),axis=1)/6.0
-sumf = np.flip(sum)
+A = 3  # paper size (affects plot dpi)
 
-plt.grid()
+lm = -3 # clamp values below this level
+plt.rc('figure', figsize=[46.82 * .5**(.5 * A), 33.11 * .5**(.5 * A)])
 
 p1 = np.log10(Pxx[c-r:c+r,:])
 p1f = np.flip(p1,0)  # flip array along 1st axis
-pdif = p1f - (p1*0.8)
+pdif = p1f - (p1*0.65)
 p2 = np.maximum(lm,pdif)
+plt.grid()
 plt.imshow(p2,cmap='magma')
 plt.show
 
