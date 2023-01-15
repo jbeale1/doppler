c148fca85b692296ea1142b1f7cd0d11dae73940
John Beale <7452231+jbeale1@users.noreply.github.com>
2023-01-14 22:48:48 -0800

    create spectrogram separate from plot

---
+++ plotIQ-2.py
@@ -0,0 +1,315 @@
+"""
+Plot Radar I/Q data on spectrogram
+Python3 code with scipy, numpy, matplotlib
+Based on FFT example at
+https://docs.scipy.org/doc/scipy/tutorial/fft.html
+
+J.Beale, Jan.14 2023
+"""
+
+
+import sys         # command-line arguments
+import os
+from scipy.fft import fft, fftfreq, fftshift
+from scipy.signal import cosine
+from scipy.io import wavfile
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2  # OpenCV for processing detected events
+
+
+#from scipy import signal
+#from scipy.io import wavfile
+#import scipy.io
+#import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd  # dataframes for output
+# import librosa # only for reading .mp3 files
+
+import datetime # to show today's date
+from skimage import morphology
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.color import label2rgb
+
+
+N = 4096       # size of FFT
+slices = 6566  # how many separate segments we do FFT on, in input function
+
+mphPerHz = 1.0/72.05 # mph/Hz
+mpsPerHz = 6.205E-3  # m/s per Hz
+fpm = 3.28084  # how many feet in a meter
+
+
+def doOneImage(fname_in):
+
+    fs, datraw = wavfile.read(fname_in) # load file with scipy
+    T = 1.0 / fs # sample interval, (s)
+    Nsamp,ch = datraw.shape
+    
+    xR = datraw[:,:].astype(np.float32) / 65535.0
+    #xR = datraw[int(Nsamp*0.03):int(Nsamp*0.06),:].astype(np.float32) / 65535.0
+    y = xR[:,0] + 1j * xR[:,1]  # complex from 2 reals
+    
+    Ntot = y.size
+    # x = np.linspace(0.0, T*Ntot, Ntot, endpoint=False) # full time axis
+    
+    xf = fftfreq(N, T)  # calculate X axis vector in time units
+    xf = fftshift(xf)   # shift FFT output to be symmetric around 0
+    w = cosine(N) # windowing function
+    
+    sgram = np.zeros((N,slices))
+    for i in range(slices):
+        a = int(i * ((Ntot - N)/slices))
+        b = a+N
+        ySlice = y[a:b]  # segment of data to do this FFT on
+        ywf = fft(ySlice*w)  # windowed FFT
+        ywf = fftshift(ywf)  # put zero freq on center instead of edge
+    
+        #ywf = np.maximum(0.01,ywf)  # clamp small noise levels to fixed value
+        #ylog = 20*np.log10(np.abs(ywf)) # convert to dB, 20log10()
+        #sgram[:,i] = ylog  # add this line to spectrogram image
+        sgram[:,i] = np.abs(ywf)  # add this line to spectrogram image
+    
+    # sgram is (N x slices) in size
+    # need to smooth over time and freq for reasonable plot
+    #fRange = int(N/5)   # what part of full (0..N/2) frequency range to show
+    fRange = 600
+    a=int((N/2)-fRange)
+    b=int((N/2)+fRange)
+    p1 = sgram[a:b,:]
+    
+    """
+    plt.imshow(20*np.log10(p1), interpolation='none')
+    #plt.imshow(sgram, interpolation='bicubic')
+    plt.show()
+    """
+    
+    # convert matrix to OpenCV plot
+    
+    fMask = 3
+    pMin = np.amin(p1)
+    pMax = np.amax(p1)
+    pRange = pMax - pMin
+    p1[fRange-fMask:fRange+fMask,:]=pMin  # mask off low frequencies to min value
+    
+    minV = 0.06
+    minT = 4.0E-3   # clamp to this minimum threshold
+    
+    p1 = (p1 - pMin) / pRange # normalize range to (0..1)
+    
+    p1f = np.flip(p1,0)  # flip array along 1st axis
+    pL = p1f - (p1*minV) # subtract off residual from inexact phase shift
+    pL = np.maximum(minT,pL) # clamp to positive definite
+    
+    p2 = 20*np.log10(pL) # units of dB
+    
+    #plt.imshow(p2, interpolation='none')
+    #plt.imshow(sgram, interpolation='bicubic')
+    #plt.show()
+    
+    pMin = np.amin(p2)
+    pMax = np.amax(p2)
+    pRange = pMax - pMin
+    
+    # plotIQ_1 setup: (fbins 1200, timebins 6566)
+    
+    # image is 6566 x 1638 pixels
+    img = np.array((p2-pMin)*255.0/pRange).astype('uint8')
+    
+    # print('Image Dimensions : ', img.shape) # (fbins 1200, timebins 6566)
+    
+    Vsize = 7  # vertical motion blur length
+    Hsize = 5  # horiz. motion blur
+    kernel_motion_blur = np.zeros((Vsize, Vsize))
+    kernel_Hmotion_blur = np.zeros((Hsize, Hsize))
+    
+    kernel_Hmotion_blur[int((Hsize-1)/2),:] = np.ones(Hsize) # H motion
+    kernel_Hmotion_blur /= Hsize
+    
+    kernel_motion_blur[:,int((Vsize-1)/2)] = np.ones(Vsize) # V motion
+    kernel_motion_blur /= Vsize
+    imgT = cv2.filter2D(img, -1, kernel_motion_blur)
+    imgB = cv2.filter2D(imgT, -1, kernel_Hmotion_blur)
+    
+    image = imgB # size = (1200, 6567)  (freq x time)
+    
+    # =====================================================
+    fig, ax =  plt.subplots(2)  # set up matplotlib plot
+
+
+    gap = 150  # don't sum near f=zero (rain noise)
+    Pstack = np.sum(image[0:(fRange-gap),:], axis=0) # + freq, sum vertically 
+    Nstack = np.sum(image[(fRange+gap):,:], axis=0) # - freq, sum vertically 
+    # ax[2].plot(Pstack)  # vertical sums
+    # ax[2].plot(Nstack)  # vertical sums
+
+    #thresh = 22  # if there is no rain
+    thresh = 45  # if there is rain (was 32)  # *Is this correct? *
+
+    bw = morphology.closing(image > thresh, morphology.square(3))
+    #cleared = bw
+    mask = morphology.remove_small_objects(bw, 800, connectivity=2)
+
+    # label image regions
+    label_image = label(mask, background=0)
+    props1 = regionprops(label_image)
+    ecount = len(props1)
+    print("Found %d events" % ecount)
+    image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+
+    ax[0].imshow(image_label_overlay)
+    ax[0].set_title('labelled image')
+    # ax[1].axis('off')
+
+
+    maskImg = (mask * 255).astype('uint8')
+    imgOut = image * (mask > 0)  # image with non-event background masked off
+
+    (fTotal, colTotal) = image.shape  # dimensions of image
+    # Time Scale Factor =  0.045689  = 300 sec / 6565 pixel columns
+    tScaleFac = (Nsamp/fs)/colTotal       # convert horizontal image pixels to time (sec)
+    eCount = 0
+    #print("n, mph, time, duration")
+
+    pd.options.display.float_format = '{:,.1f}'.format
+    df = pd.DataFrame(columns = 
+         ['mphmax','mphavg','mphmin', 'stdAvg', 'area', 'len',
+            'time', 'dist', 'dur', 'type'])
+    mphScale = (fs/N) * mphPerHz  # to get units of mph
+    mpsScale = (fs/N) * mpsPerHz  # to get units of m/s    
+
+    for region in props1:
+        #print(region.area)
+        area = region.area
+        if area >= 700:  # was 1200 draw a bounding rectangle
+            eType = 'car' # by default, unless found otherwise
+            fCenter = region.centroid[0]                                  
+            minr, minc, maxr, maxc = region.bbox
+            imgS = imgOut[minr:maxr,minc:maxc] # image selected region
+
+            eDur = (maxc-minc) * tScaleFac  # event duration in units of seconds
+            # apt = area / eDur # total pixel area per unit time
+            peaks = np.argmax(imgS, axis=0) # max along 1st axis
+            vVec = (fRange-(minr + peaks)) * mpsScale # velocity in m/s at each moment in time
+            mDist = np.trapz(vVec) * tScaleFac * fpm  # total distance travelled (in feet)
+            avgP = minr + np.average(peaks) # average of all values
+            pkSz = peaks.size  # number of elements in vector
+            pk1 = peaks[0:int(pkSz/3)]
+            pk2 = peaks[int(pkSz/3):2*int(pkSz/3)]
+            pk3 = peaks[2*int(pkSz/3):]
+            std1 = np.std(np.diff(pk1[0::2]))    # measure of stability of velocity
+            std2 = np.std(np.diff(pk2[0::2]))    # measure of stability of velocity
+            std3 = np.std(np.diff(pk3[0::2]))    # measure of stability of velocity
+            #peakP = minr + np.amin(peaks) # index of highest frequency (if pos.)
+            Svec = abs(fRange - (minr + peaks)) * mphScale
+            posFreq = False # just a default
+            
+            # print(peaks.size, peaks)
+            if (minr < fRange): # positive frequency half of plot
+              maxP = minr + np.amin(peaks) # index of highest frequency (if pos.)                  
+              minP = minr + np.amax(peaks) # index of lowest frequency (if pos.)                  
+              eTime = maxc * tScaleFac  # time in seconds
+              stdAvg = (std1+std2)/2
+              posFreq = True
+            if (maxr > fRange): # negative frequency, bottom half of plot
+              maxP = minr + np.amax(peaks) # index of highest frequency (if neg.)                            
+              minP = minr + np.amin(peaks) # index of highest frequency (if neg.)                            
+              eTime = minc * tScaleFac  # start time in seconds
+              stdAvg = (std2+std3)/2
+              posFreq = False
+
+            mphMax = (fRange - maxP) * mphScale
+            mpsMax = (fRange - maxP) * mpsScale
+            mphAvg = (fRange - avgP) * mphScale
+            mpsAvg = (fRange - avgP) * mpsScale
+            mphMin = (fRange - minP) * mphScale
+            #print(mpsAvg)
+            stdAvg *= 5.0/abs(mphAvg) # scaled by avg speed
+            #mphStd = std * mphScale  # stdAvg was 8
+            if ((eDur > 2) and (stdAvg < 8) and (abs(mphMax) > 2) and (abs(mphMax) > 1.4) and 
+                  ((abs(mphMax) > 5) or (eDur > 5))): # skip any slow events if too short
+              if ( (abs(mphMax) > 5) and ((stdAvg > 0.75) or (mDist > 300)) ):
+                  eType = 'o_car' # probably combined events of some kind
+
+              if ( (abs(mphMax) < 5) and (stdAvg > 2) ):
+                  eType = 'ped' # by default, unless found otherwise
+
+              # ax[2].plot(Svec)  # plot V vs T profile
+              
+              if (posFreq):
+                  sig = Pstack[minc:maxc]
+              else:
+                  sig = Nstack[minc:maxc]
+              sig = sig - (np.amax(sig)*0.7)
+              pkCount = np.sum(sig > 0) # width of peak ~ length of vehicle
+              length = int(pkCount * tScaleFac * abs(mpsMax) * fpm) # in feet
+              ax[1].plot(sig)  # vertical sums
+              ax[1].grid()              
+              
+                # add this event to dataframe
+              df.loc[eCount] = [mphMax, mphAvg, mphMin, stdAvg, area, length,
+                                eTime, mDist, eDur, eType]
+              eCount += 1
+              # add visible box around detected event on graph
+              rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                      fill=False, edgecolor='red', linewidth=1)
+              ax[0].add_patch(rect)
+
+              
+
+    df = df.sort_values(by=['time'])  # events in time order of appearance
+    df = df.reset_index(drop=True)  # reset the index to be in sorted time order
+    
+    
+    #fname_out1 = "test_out.png"
+    #cv2.imwrite(fname_out1,image) # detected image
+    
+    if (savePlot):        
+        #out = image * (mask > 0)
+        fbase = os.path.basename(fname_in)
+        if ( fbase[-4:] == '.wav'):  # remove the .wav extension
+            fbase = fbase[:-4]
+        fname_out1= fdirOut + fbase + "_3.png"
+        fname_out2= fdirOut + fbase + "_mask.png"
+        cv2.imwrite(fname_out1,image) # detected image
+        #cv2.imwrite(fname_out1,imgOut) # detected image
+        cv2.imwrite(fname_out2,maskImg) # peaks mask
+    
+    
+    #cv2.imshow("spectrogram", image)
+    #cv2.waitKey(0)
+    return df
+
+# ===================================================================    
+# Main program starts here  
+  
+showPlot = True  # show spectrogram graphs
+savePlot = True
+
+fdirOut = "./"
+#wdir="C:/Users/beale/Documents/Audio/"
+#fname_in = wdir + "DpD_2023-01-14_16-55-00.wav"
+#fname_in = wdir + "DpD_2023-01-14_12-35-00.wav"  
+#doOneImage(fname_in)
+
+#fname1 = "DpD_2023-01-14_16-55-00"
+fname1 = "DpD_2023-01-14_12-35-00"
+
+fdir="C:/Users/beale/Documents/Audio/"
+#fname_in = wdir + "DpD_2023-01-14_16-55-00.wav"
+
+fname1 = fdir + fname1
+if ( fname1[-4:] != '.wav'):
+    fname1 += '.wav'
+
+df = doOneImage(fname1)
+
+eCount = len(df.index)
+
+today = datetime.date.today()
+dstring = today.strftime('%Y-%b-%d')
+print("%s at %s  Total events: %d" % (fname1, dstring, eCount))
+print(df)
